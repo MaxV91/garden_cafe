dac7fd82a7603c8952bd2b9a76d17322db978a78
Maxim K <max.kotsurenko@gmail.com>
2022-04-26 19:01:45 +0300

    init

--- src/components/ContactsMainView.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const ContactsMainView: React.FC = () => {
-  return (
-    <div>Contacts</div>
-  );
-};
-
-export default ContactsMainView;
--- src/components/MainView.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const MainView: React.FC = () => {
-  return (
-    <>Main View</>
-  );
-};
-
-export default MainView;
